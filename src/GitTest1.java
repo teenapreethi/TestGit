fb807756e3b2bb99276100029fecf986c8a75029
Auxiliajudy <auxilia_judy@yahoo.com>
2018-04-27 09:54:33 +0530

    added a new line

--- src/GitTest1.java
+++ src/GitTest1.java
@@ -1,6 +1,8 @@
 
 public class GitTest1 {
-public static void main(String[] args) {
-	System.out.println("Adding file to git");
-}
+	public static void main(String[] args) {
+		System.out.println("Adding file to git");
+		System.out.println("Changes made to see the difference");
+		System.out.println("Adding new line");
+	}
 }
